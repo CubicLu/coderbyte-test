95f47492989f8c218f7b88e404e5c1174fb4ee45
Oleksii Anokhin <anderia.su@gmail.com>
2021-08-18 13:44:43 +0300

    app bootstrap

---
+++ coderbyte-app/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    filepath: '**/*.js',
+    printWidth: 120,
+    semi: false,
+    tabWidth: 2,
+    parenthesisSpace: true,
+  }
+  
